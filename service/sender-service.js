b19c068496d2a10c9f6a5bb9286512ebd5e94b42
Vlad Gerasimchuk <88591743+2Voby@users.noreply.github.com>
2024-01-16 20:35:20 +0200

    Add files via upload

--- service/sender-service.js
+++ service/sender-service.js
@@ -43,8 +43,8 @@ class SendService {
     message += `Автор: ${postContent.author}\n`;
     message += `<a href="${postContent.postLink}">Переглянути всю інформацію</a>\n`;
 
-    // let chat_ids_arr = [CHANNEL_CHAT_ID, TEACHERS_CHAT_ID]; // prod
-    let chat_ids_arr = [CHANNEL_CHAT_ID]; // dev
+    let chat_ids_arr = [CHANNEL_CHAT_ID, TEACHERS_CHAT_ID]; // prod
+    // let chat_ids_arr = [CHANNEL_CHAT_ID]; // dev
 
     let response;
     for (const CHAT_ID of chat_ids_arr) {
@@ -64,8 +64,8 @@ class SendService {
     const TEACHERS_CHAT_ID = -4069606119;
     const URI_API_SEND_PHOTO = `https://api.telegram.org/bot${TOKEN}/sendPhoto`;
 
-    // let chat_ids_arr = [CHANNEL_CHAT_ID, TEACHERS_CHAT_ID]; // prod
-    let chat_ids_arr = [CHANNEL_CHAT_ID]; // dev
+    let chat_ids_arr = [CHANNEL_CHAT_ID, TEACHERS_CHAT_ID]; // prod
+    // let chat_ids_arr = [CHANNEL_CHAT_ID]; // dev
 
     let response;
     for (const CHAT_ID of chat_ids_arr) {
